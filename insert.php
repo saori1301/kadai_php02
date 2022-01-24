7c5c98e056516f40f63c76d3195cf6c73e32e428
saori1301 <saori1301@gmail.com>
2022-01-24 17:59:44 +0900

    first commit

---
+++ insert.php
@@ -0,0 +1,41 @@
+<?php
+//1. POSTデータ取得
+$name = $_POST["name"];
+$url = $_POST["url"];
+$naiyou = $_POST["naiyou"];
+
+
+//2. DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=gs_db2;charset=utf8;host=localhost','root','root');
+} catch (PDOException $e) {
+  exit('DBConnectError:'.$e->getMessage());
+}
+
+
+//３．SQL文を用意(データ登録：INSERT)
+$stmt = $pdo->prepare(
+  "INSERT INTO gs_bk_table( id, name, url, naiyou, indate )
+  VALUES( NULL, :name, :url, :naiyou, sysdate() )"
+);
+
+// 4. バインド変数を用意
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':url', $url, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':naiyou', $naiyou, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+
+// 5. 実行
+$status = $stmt->execute();
+
+
+//6．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("ErrorMassage:".$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+  header('Location: index.php');//ヘッダーロケーション（リダイレクト）
+}
+?>
---
+++ select.php
@@ -0,0 +1,69 @@
+<?php
+
+//1.  DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=gs_db2;charset=utf8;host=localhost','root','root');
+} catch (PDOException $e) {
+  exit('DBConnectError:'.$e->getMessage());
+}
+
+//２．SQL文を用意(データ取得：SELECT)
+$stmt = $pdo->prepare("SELECT * FROM gs_bk_table");
+
+//3. 実行
+$status = $stmt->execute();
+
+//4．データ表示
+$view="";//空のviewを作成
+if($status==false) {
+    //execute（SQL実行時にエラーがある場合）
+  $error = $stmt->errorInfo();
+  exit("ErrorQuery:".$error[2]);
+
+}else{
+  //Selectデータの数だけ自動でループしてくれる
+  //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+  while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){ 
+    $view .= "<p>";
+    $view .= $result['indate'].':'.$result['name'].' '.$result['url'].' '.$result['naiyou'];
+    $view .= "</p>";
+  }
+
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>フリーアンケート表示</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">データ登録</a>
+      </div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<div>
+    <div class="container jumbotron"><?= $view ?></div>
+</div>
+<!-- Main[End] -->
+
+</body>
+</html>
